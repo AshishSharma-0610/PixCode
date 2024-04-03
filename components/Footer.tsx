304eccfd03340b23892749c661d8fa11f9e1d086
AshishSharma-0610 <136923510+AshishSharma-0610@users.noreply.github.com>
2024-04-04 04:35:48 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,8 +6,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="flex items-center gap-10 py-16">
-        <span className="pr-2">Made by </span>
-        <span className="pl-1 font-medium text-slate-200">Ashish Sharma</span>
+        <span className="pr-2">Made by <span className="pl-1 font-medium text-slate-200">Ashish Sharma</span></span>
       <Link
         href="https://github.com/AshishSharma-0610/PixCode"
         className="flex items-center text-sm font-medium hover:text-slate-200 ease-in-out transition-all duration-500"
